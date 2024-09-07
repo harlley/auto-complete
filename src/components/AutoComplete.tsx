918e55fe455d44cc642189c9bfa15f3ab9340d8a
Harlley Oliveira <harlley@gmail.com>
2024-09-07 10:50:41 +0100

    add clear action

--- src/components/AutoComplete.tsx
+++ src/components/AutoComplete.tsx
@@ -34,6 +34,9 @@ export function AutoComplete({
           {...inputProps}
         />
         {isLoading && <Spinner />}
+        {inputValue.length > 0 && !isLoading && (
+          <ClearIcon setInputValue={setInputValue} />
+        )}
       </div>
 
       {filteredOptions.length > 0 && (
@@ -53,6 +56,31 @@ export function AutoComplete({
   );
 }
 
-export function Spinner() {
+function Spinner() {
   return <div className={styles.spinner}></div>;
+}
+
+interface ClearIconProps {
+  setInputValue: (value: string) => void;
+}
+
+function ClearIcon({ setInputValue }: ClearIconProps) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      width="20"
+      height="20"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className={styles.clearIcon}
+      onClick={() => setInputValue("")}
+    >
+      <line x1="18" y1="6" x2="6" y2="18" />
+      <line x1="6" y1="6" x2="18" y2="18" />
+    </svg>
+  );
 }
